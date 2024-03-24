eabdec6475cbacd708bffda714cf3becc540e0ca
Adam Darr <darr75adam@gmail.com>
2024-03-24 13:40:00 -0500

    new file writer in Groovey
    it said I take to Groovey

---
+++ ATD Codes/amyInteligence.js
@@ -0,0 +1,13 @@
+mobile + {amyInteligence} 
+ {cardsDealer} let (H) = {amyInteligence} 
+ let (H) + mobileGaming; {gamepad} 
+ ConstantSourceNode; {amyInteligence}  mobileData;
+ amyInteligence (URL) 
+ amyInteligence; {girlCharacter} mobileGaming
+ (amyInteligence) = {years} (8)
+ personality; {amyInteligence} {lovesCards}
+ (hairColor) = {red}
+ habits = {tobaccoUse};
+ {knows-rules-of-poker} {H} {smoking-A-Pipe} {H}
+ mobileData; {MediaSource} 
+let array; {amyInteligence}
